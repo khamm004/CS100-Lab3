67fbbed4da64772952b68ceaee8c32476879292a
khamm004 <khamm004@ucr.edu>
2021-01-21 14:43:44 -0800

    Added Op tests for evaluate and stringify for nonzero, zero, negative and large inputs

--- op_test.hpp
+++ op_test.hpp
@@ -10,4 +10,31 @@ TEST(OpTest, OpEvaluateNonZero) {
     EXPECT_EQ(test->evaluate(), 8);
 }
 
+TEST(OpTest, OpStringifyNonZero) {
+    Op* test = new Op(8);
+    EXPECT_EQ(test->stringify(), "8.000000");
+}
+
+
+TEST(OpTest, OpEvaluateZero) {
+    Op* test = new Op(0);
+    EXPECT_EQ(test->evaluate(), 0);
+}
+
+TEST(OpTest, OpStringifyZero) {
+    Op* test = new Op(0);
+    EXPECT_EQ(test->stringify(), "0.000000");
+}
+
+
+TEST(OpTest, OpEvaluateNegative) {
+    Op* test = new Op(-7.5);
+    EXPECT_EQ(test->evaluate(), -7.5);
+}
+
+TEST(OpTest, OpStringifyNegative) {
+    Op* test = new Op(-7.5);
+    EXPECT_EQ(test->stringify(), "-7.500000");
+}
+
 #endif //__OP_TEST_HPP__
