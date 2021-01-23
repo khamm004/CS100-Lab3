633faf01771862d4eab0f0964dd85e5bb572d8b5
khamm004 <khamm004@ucr.edu>
2021-01-23 11:58:12 -0800

    added set of evaluate/stringify mult tests for numbers with diffrent signs

--- mult_test.hpp
+++ mult_test.hpp
@@ -38,6 +38,21 @@ TEST(MultTest, MultStringifyBothNeg) {
     EXPECT_EQ(test->stringify(), "(-5.000000*-2.000000)");
 }
 
+TEST(MultTest, MultEvaluateDifSigns) {
+    Op* right = new Op(-2);
+    Op* left = new Op(5);
+    Mult* test = new Mult(left, right);
+
+    EXPECT_EQ(test->evaluate(),-10.000000 );
+}
+
+TEST(MultTest, MultStringifyDifSigns) {
+    Op* right = new Op(-2);
+    Op* left = new Op(5);
+    Mult* test = new Mult(left, right);
+
+    EXPECT_EQ(test->stringify(), "(5.000000*-2.000000)");
+}
 
 #endif //__MULT_TEST_HPP__
 
