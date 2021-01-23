02cd5d27aa404c9ad2af672ef08b97176590e1d7
khamm004 <khamm004@ucr.edu>
2021-01-23 11:52:58 -0800

    created first set of evaluate/stringify mult tests for two positive numbers

---
+++ mult_test.hpp
@@ -0,0 +1,26 @@
+#ifndef __MULT_TEST_HPP__
+#define __MULT_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "op.hpp"
+#include "mult.hpp"
+
+TEST(MultTest, MultEvaluateBothPos) {
+    Op* right = new Op(2);
+    Op* left = new Op(5);
+    Mult* test = new Mult(left, right);
+    
+    EXPECT_EQ(test->evaluate(),10.000000 );
+}
+
+TEST(MultTest, MultStringifyBothPos) {
+    Op* right = new Op(2);
+    Op* left = new Op(5);
+    Mult* test = new Mult(left, right);
+
+    EXPECT_EQ(test->stringify(), "(5.000000*2.000000)");
+}
+
+#endif //__MULT_TEST_HPP__
+
