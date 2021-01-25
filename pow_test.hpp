de5077791687b67e5d8ce75774dbb6a791a8800c
glee139 <glee00328@gmail.com>
2021-01-25 13:56:26 -0800

    added tests that cross diff classes

--- pow_test.hpp
+++ pow_test.hpp
@@ -1,10 +1,13 @@
 #ifndef __POW_TEST_HPP__
 #define __POW_TEST_HPP__
-
+#include <cmath>
 #include "gtest/gtest.h"
+#include <iostream>
 using namespace std;
-
+#include "div.hpp"
+#include "mult.hpp"
 #include "pow.hpp"
+#include "sub.hpp"
 #include "op.hpp"
 
 TEST(PowTest, PowEvaluatePositive) {
@@ -61,23 +64,44 @@ TEST(PowTest, PowStringifyNegNeg) {
 	Op* nTwo = new Op(-2);
     Pow* test = new Pow(nTwo, nThree);
     EXPECT_EQ(test->stringify(), "(-2.000000**-3.000000)");
-};
+}
+TEST(PowTest, PowEvaluateNAN) {
+	Op* A = new Op(-3.5);
+    Pow* test = new Pow(A, A);
+    cout << "Invalid output" << endl;
+}
 
-//TEST(PowTest, PowEvaluateWMult) {
-//   Op* three = new Op(3);
-//   Op* two = new Op(2);
-//   Op* five = new Op(5)
-//   Mult* A = new Mult(three, two)
-//   Pow* test = new Pow(A, five);
-//   EXPECT_EQ(test->evaluate(), 7776);
-//}
-//
-//TEST(PowTest, PowStringifyWMult) {
-//    Op* three = new Op(3);
-//    Op* two = new Op(2);
-//    Op* five = new Op(5)
-//    Mult* A = new Mult(three, two)
-//    Pow* test = new Pow(A, five);
-//    EXPECT_EQ(test->evaluate(), "((3.000000*2.000000)**5.000000)");
-//}
+TEST(PowTest, PowEvaluateInvalid) {
+        Op* nTwo = new Op(-2);
+	Op* fraction = new Op(1.5);
+    Pow* test = new Pow(nTwo, fraction);
+    cout << "Invalid output"<< endl;
+}
+
+TEST(PowTest, PowEvaluateWDiv) {
+	Op* five = new Op(5);
+	Op* two = new Op(2);
+  Div* A = new Div(five, two);
+  Pow* test = new Pow(A, two);
+  EXPECT_EQ(test->evaluate(), 6.25);
+}
+
+TEST(PowTest, DivEvaluateWPowSub) {
+	Op* three = new Op(3);
+	Op* two = new Op(2);
+	Op* four = new Op(4);
+  Pow* A = new Pow(two, three);
+	Sub* B = new Sub(A,four);
+  Div* test = new Div(B, two);
+  EXPECT_EQ(test->evaluate(), 2);
+}
+
+TEST(PowTest, PowEvaluateWMult) {
+   Op* three = new Op(3);
+   Op* two = new Op(2);
+   Op* five = new Op(5);
+   Mult* A = new Mult(three, two);
+   Pow* test = new Pow(A, five);
+   EXPECT_EQ(test->evaluate(), 7776);
+};
 #endif
--- sub_test.hpp
+++ sub_test.hpp
@@ -1,9 +1,13 @@
+ 
 #ifndef __SUB_TEST_HPP__
 #define __SUB_TEST_HPP__
 
 #include "gtest/gtest.h"
 using namespace std;
 
+
+#include "div.hpp"
+#include "mult.hpp"
 #include "sub.hpp"
 #include "op.hpp"
 
@@ -61,24 +65,44 @@ TEST(SubTest, SubStringifyNegNeg) {
 	Op* nTwo = new Op(-2);
     Sub* test = new Sub(nTwo, nThree);
     EXPECT_EQ(test->stringify(), "(-2.000000--3.000000)");
-};
+}
 
-//TEST(SubTest, SubEvaluateWMult) {
-//    Op* three = new Op(3);
-//    Op* two = new Op(2);
-//    Op* five = new Op(5)
-//    Mult* A = new Mult(three, two)
-//    Sub* test = new Sub(A, five);
-//    EXPECT_EQ(test->evaluate(), 1);
-//}
-//
-//TEST(SubTest, SubStringifyWMult) {
-//    Op* three = new Op(3);
-//    Op* two = new Op(2);
-//    Op* five = new Op(5)
-//    Mult* A = new Mult(three, two)
-//    Sub* test = new Sub(A, five);
-//    EXPECT_EQ(test->evaluate(), "((3.000000*2.000000)-5.000000)");
-//}
+TEST(SubTest, SubEvaluateWMult) {
+    Op* three = new Op(3);
+    Op* two = new Op(2);
+    Op* five = new Op(5);
+    Mult* A = new Mult(three, two);
+    Sub* test = new Sub(A, five);
+    EXPECT_EQ(test->evaluate(), 1);
+}
+
+TEST(SubTest, SubEvaluateWMultDiv) {
+    Op* eight = new Op(8);
+    Op* two = new Op(2);
+    Op* four = new Op(4);
+    Op* six =new Op(6);
+    Mult* A = new Mult(eight, two);
+    Sub* B = new Sub(A, four);
+    Div* test=new Div(B,six);
+    EXPECT_EQ(test->evaluate(), 2);
+}
+
+TEST(SubTest, MultEvaluateWSub) {
+    Op* three = new Op(3);
+    Op* two = new Op(2);
+    Op* five = new Op(5);
+    Sub* A = new Sub(three, two);
+    Mult* test = new Mult(A, five);
+    EXPECT_EQ(test->evaluate(), 5);
+}
+
+TEST(SubTest, SubStringifyWMult) {
+    Op* three = new Op(3);
+    Op* two = new Op(2);
+    Op* five = new Op(5);
+    Mult* A = new Mult(three, two);
+    Sub* test = new Sub(A, five);
+    EXPECT_EQ(test->stringify(), "((3.000000*2.000000)-5.000000)");
+};
 
 #endif
